2b2442b34a3ba4339f06af5f214d539049d889a3
Luciano Oliveira <luciano101@msn.com>
2017-12-16 20:08:04 -0800

    add versioning

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :parks
+  # namespace the controllers without affecting the URI
+  # scope module: :v1, constraints: ApiVersion.new('v1', true) do
+  #   resources :parks
+  # end
+  namespace 'v1' do
+    resources :parks
+  end
 end
